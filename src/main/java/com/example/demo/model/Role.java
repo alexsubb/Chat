2950819d9031bfb66e9f6e173f36a7fa1811d8b8
alex <subbotin-as@bisys.ru>
2019-03-31 18:49:19 +0500

    Добавил регистрацию и авторизацию пользователей

---
+++ src/main/java/com/example/demo/model/Role.java
@@ -0,0 +1,35 @@
+package com.example.demo.model;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", users=" + users +
+                '}';
+    }
+}
